ed3e8ed86bbd8dff8e2e78a6bd2f8e578c325003
David Shibru <davshibru@gmail.com>
2022-07-19 19:37:22 +0600

    fix bugs

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -23,7 +23,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
-            Debug.Log(“Game Over!”)
+            Debug.Log("Game Over!");
         }
     }
 }
