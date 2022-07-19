542d8208e50017f68dd0aa0be2a2ec469088571e
David Shibru <davshibru@gmail.com>
2022-07-19 19:36:18 +0600

    complete all

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject projectilePrefab;
+
+
     public float xRange = 10;
     public float horizontalInput;
     public float speed = 10.0f;
@@ -28,6 +31,12 @@ public class PlayerController : MonoBehaviour
         
 
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 
 }
